b8a8bfa364496c63ede073aa03f6985d268b65fa
aziznosirov <67833332+aziznosirov@users.noreply.github.com>
2024-02-14 18:18:41 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,1 +1,15 @@
-def getPrimeNumbers(n):
+def getPrimeNumbers(n): 
+    prime_numbers = [num for num in range(2, n+1) if is_prime(num)]
+    print(prime_numbers)
+    
+def is_prime(num):
+    if num == 2:
+        return True
+    
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+
+    return True
+    
+
